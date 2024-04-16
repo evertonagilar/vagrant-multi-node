8aebb5b8afe154b5e3c960f0c0953125b3a0337d
Everton de Vargas Agilar <evertonagilar@gmail.com>
2024-04-16 18:31:20 -0300

    v1.0.0

--- Vagrantfile
+++ Vagrantfile
@@ -1,22 +1,23 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-ENV['VAGRANT_NO_PARALLEL'] = 'yes'
+IP_START = 1
 
 Vagrant.configure(2) do |config|
 
-  config.vm.provision "shell", path: "bootstrap.sh"
-  config.vm.box = "bento/debian-12"
-  config.vm.box_version = "202401.31.0"
+  config.vm.box = "boxomatic/debian-12"
+  config.vm.box_version = "20240225.0.1"
   config.vm.box_check_update = false
+  config.vbguest.auto_update = false
+  config.vm.provision "shell", path: "bootstrap.sh"
 
-  Count = 3
+  Count = 2
 
   # Cria as VMs
   (1..Count).each do |i|
       config.vm.define "node#{i}" do |node|
         node.vm.hostname = "node#{i}"
-        node.vm.network "private_network", ip: "192.168.10.10#{i}"
+        node.vm.network "private_network", ip: "192.168.10.#{IP_START + i}"
         node.vm.provider "virtualbox" do |v|
           v.name = "node#{i}"
           v.memory = 2048
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,51 @@
 #!/bin/bash
 
-echo 'Configurando comunicação ssh entre VMs'
-echo "Enable ssh password authentication"
+echo Configurar comunicação ssh entre VMs
 sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 systemctl reload sshd
-
 echo 'Definindo senha do root para teste'
 echo -e "teste\nteste" | passwd root >/dev/null 2>&1
 
-echo 'Configura o vim'
+
+### --------------------------------
+
+
+echo Configurar o locale pt_BR.UTF-8
+export LANG=pt_BR.UTF-8
+echo "LANG=${LANG}" |  tee -a /etc/environment
+sed -i "s/# pt_BR.UTF-8 UTF-8/${LANG} UTF-8/" /etc/locale.gen
+locale-gen "${LANG} UTF-8"
+
+
+### --------------------------------
+
+
+echo Configurar o timezone para America/Sao_Paulo
+echo America/Sao_Paulo |  tee /etc/timezone
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+
+### --------------------------------
+
+
+echo Instalação dos pacotes necessários para a utilização do Kubernetes
+apt install -y vim apt-transport-https ca-certificates curl wget gnupg lsb-release nfs-common htop chrony
+
+
+### --------------------------------
+
+
+echo Criar /etc/apt/keyrings se não existe
+mkdir -p /etc/apt/keyrings
+chmod 755 -R /etc/apt/keyrings
+
+
+### --------------------------------
+
+
+echo Configurar o vim
 cat << EOF > /root/.vimrc
 set nomodeline
 set bg=dark
@@ -20,3 +56,9 @@ set nu
 syntax on
 EOF
 cp /root/.vimrc /home/vagrant/.vimrc && chown vagrant:vagrant /home/vagrant/.vimrc
+
+
+### --------------------------------
+
+
+reboot
